a8669f621252f33a02b4b963051dfd1000bc60c8
Marlon <marlonildofm@gmail.com>
2022-09-27 12:17:38 -0300

    Desafio POO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"O seu IAplicativo {nomeApp} foi instalado através da Iloja no seu Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia tijolo");
+Smartphone nokia = new Nokia("999888777", "Nokia tijolin modelo 1 azul", "abcd1234", 10);
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo da cobrinha");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("0110010101", "Iphone sem fone e sem carregador", "XYZYXYZX", 30);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("I-apps");
